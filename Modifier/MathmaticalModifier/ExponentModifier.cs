401c19d5c4f9182252eab6655f569ed5d9f6e5a4
dadaatom <11johnson1111@gmail.com>
2022-07-31 19:53:05 -0400

    Added ExponentModifier.cs.

---
+++ Modifier/MathmaticalModifier/ExponentModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modifiers.Modifier.MathmaticalModifier
+{
+    public class ExponentModifier : global::Modifier
+    {
+        public float Value { get; }
+    
+        public ExponentModifier(float value) : this("", 0, value) { }
+
+        public ExponentModifier(string label, float value) : this(label, 0, value) { }
+    
+        public ExponentModifier(string label, int priority, float value) : base(label, priority)
+        {
+            Value = value;
+        }
+    
+        public override float Compute(float input)
+        {
+            return (float)Math.Pow(input, Value);
+        }
+    }
+}
